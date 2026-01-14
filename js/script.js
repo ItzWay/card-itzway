600ee0f53c71355a8f15b44005bf82da971cea60
ItzWay <130004175+ItzWay@users.noreply.github.com>
2026-01-14 10:42:09 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,17 +1,17 @@
-var cursor = document.querySelector('.blob');
+var cursor = document.querySelector(".blob");
 
-document.addEventListener('mousemove', function(e) {
+document.addEventListener("mousemove", function (e) {
   cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
 });
 
-document.addEventListener('touchmove', function(e) {
+document.addEventListener("touchmove", function (e) {
   if (e.touches.length > 0) {
     const touch = e.touches[0];
     cursor.style.transform = `translate3d(calc(${touch.clientX}px - 50%), calc(${touch.clientY}px - 50%), 0)`;
   }
 });
 
-window.addEventListener('mouseout', function() {
+window.addEventListener("mouseout", function () {
   const centerX = window.innerWidth / 2;
   const bottomY = window.innerHeight / 2;
   cursor.style.transform = `translate3d(calc(${centerX}px - 50%), calc(${bottomY}px - 50%), 0)`;
@@ -29,19 +29,19 @@ addFriendBtn.addEventListener("click", function () {
   friendBtn.style.display = "inline-flex"; // or "block" if needed
 });
 
-const handleButtonClick = e => {
+const handleButtonClick = (e) => {
   const targetSection = e.target.getAttribute("data-section");
   const section = document.querySelector(targetSection);
-  targetSection !== "#about" ?
-  card.classList.add("is-active") :
-  card.classList.remove("is-active");
+  targetSection !== "#about"
+    ? card.classList.add("is-active")
+    : card.classList.remove("is-active");
   card.setAttribute("data-state", targetSection);
-  sections.forEach(s => s.classList.remove("is-active"));
-  buttons.forEach(b => b.classList.remove("is-active"));
+  sections.forEach((s) => s.classList.remove("is-active"));
+  buttons.forEach((b) => b.classList.remove("is-active"));
   e.target.classList.add("is-active");
   section.classList.add("is-active");
 };
 
-buttons.forEach(btn => {
+buttons.forEach((btn) => {
   btn.addEventListener("click", handleButtonClick);
 });
